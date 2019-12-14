33ee86f5d9c1774d33aaae4f744d40f9d9a9afd2
bogdan13971 <37065789+bogdan13971@users.noreply.github.com>
2019-12-14 21:12:48 +0200

    bogdan bos

--- boot_loader.asm
+++ boot_loader.asm
@@ -1,26 +1,18 @@
-mov ah, 0x0e ;teletype interrupt
-mov al, 'H'
-int 0x10 
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, ' '
+[org 0x7c00]
+
+mov ah, 0x13; write string
+mov al, 1 ;update cursor
+mov bh, 0;bh page number
+mov bl, 0x0c ; red color
+mov cx, length
+mov dl, 20 ;dl column number
+mov dh, 10 ;dh row number
+push cs 
+pop es 
+mov bp, string ;bp points to string
 int 0x10
 
-mov al, '!'
-mov bh, 0
-mov bl, 0011b
-mov cx, 3
-mov ah, 0x09
-int 0x10; print pixel at cursor
-
-loop:
-    jmp loop ;infinte loop
-
-times 510 - ($ - $$) db 0
+string: db 'Hello this is Bogdan bOS', 0
+length: equ $ - string
+times 510-($-$$) db 0
 dw 0xaa55
